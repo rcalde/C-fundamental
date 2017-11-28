fc210b7205d99ad438d6c5504c1b19b952fda434
rcalde <raul_alejandro73@hotmail.com>
2017-11-28 10:19:47 -0600

    Are you sleepy

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your Name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get");
+            int hours = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+            
+
+            if (hours > 8)
+            {
+                Console.WriteLine("you are well rested");
+
+            }
+            else
+            {
+                Console.WriteLine("you need more sleep");
+            }
+            Console.ReadLine();
+        }
+    }
+}
