479436c80a90dd07e33effc159de24e53e4b22d5
rcalde <raul_alejandro73@hotmail.com>
2017-11-28 12:10:47 -0600

    Unit Test

---
+++ Grades/Grades.Test/GradeBookTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grades;
+
+namespace Grades.Test
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputeHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(90, result.highest);
+        }
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.lowest);
+        }
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.average,0.01);
+        }
+    }
+}
